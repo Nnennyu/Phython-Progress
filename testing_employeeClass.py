4b825181a1dfe9578dbf47b53d5a461d76bcdfbf
Nnennaya Ihekwoaba <63208375+Nnennyu@users.noreply.github.com>
2020-06-09 15:44:05 +0100

    Add files via upload

---
+++ testing_employeeClass.py
@@ -0,0 +1,8 @@
+import unittest
+from employee import Employee
+
+class TestEmployee(unittest.TestCase):
+    """Test for 'class Employee'. """
+
+    def setUp(self):
+        """Creating a method to use in all cases."""
